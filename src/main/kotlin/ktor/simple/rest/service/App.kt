e17af4ac64414fdb79dc34f09de956e818390d73
vadim-hleif <vadzim.hleif@gmail.com>
2020-12-05 18:23:23 +0300

    chore(build.gradle, CI): fix deps, add CI

--- src/main/kotlin/ktor/simple/rest/service/App.kt
+++ src/main/kotlin/ktor/simple/rest/service/App.kt
@@ -2,11 +2,7 @@ package ktor.simple.rest.service
 
 class App {
     val greeting: String
-        get() {
-            return "Hello world."
-        }
+        get() = "Hello world."
 }
 
-fun main(args: Array<String>) {
-    println(App().greeting)
-}
+fun main() = println(App().greeting)
